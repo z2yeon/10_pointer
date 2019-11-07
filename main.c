ce8aa253133d5ee4fcae4643cd95b1e77c4f57f8
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-07 16:02:36 +0900

    10_05(평균)

--- main.c
+++ main.c
@@ -3,20 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y){
+int main(void){
+	int i;
+	int grade[5];
+	int sum;
 	
-	int temp;
-	temp=*x;
-	*x=*y;
-	*y=temp;
-  
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]= ",i);
+		scanf("%d",&grade[i]);
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%d]=%d\n",i,grade[i]);
+		sum+=grade[i];
+	}
+	 
+	 
+	 
+	
+	printf("\n average=%d",sum/5);
+	 
 }
 
-void main (void){
-	
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a:%i, b:%i\n",a,b);
-	
-}
